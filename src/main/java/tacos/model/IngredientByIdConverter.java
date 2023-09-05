68c49e6708f77bdb3d27a58219635f2f89af9eb9
Alex Monk <amonk446@gmail.com>
2023-09-05 22:00:12 +1000

    Fixed type conversion error, /design page working

--- src/main/java/tacos/model/IngredientByIdConverter.java
+++ src/main/java/tacos/model/IngredientByIdConverter.java
@@ -1,12 +1,13 @@
 package tacos.model;
 
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
-public class IngredientByIdConverter {
+public class IngredientByIdConverter implements Converter<String, Ingredient> {
 
     private Map<String, Ingredient> ingredientMap = new HashMap<>();
 
