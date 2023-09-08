e0c93509d6fe6a8c021de9723bf71cb3e4b2dd70
Alex Monk <amonk446@gmail.com>
2023-09-08 20:08:00 +1000

    Added validation to order fields

--- src/main/java/tacos/model/TacoOrder.java
+++ src/main/java/tacos/model/TacoOrder.java
@@ -1,20 +1,40 @@
 package tacos.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
 import java.util.List;
 import java.util.ArrayList;
 
 @Data
 public class TacoOrder {
 
+    @NotBlank(message="Delivery name is required")
     private String deliveryName;
+
+    @NotBlank(message="Street is required")
     private String deliveryStreet;
-    private String deliveryCity;
+
+    @NotBlank(message="State is required")
     private String deliveryState;
+
+    @NotBlank(message="City is required")
+    private String deliveryCity;
+
+    @NotBlank(message="Zip code is required")
     private String deliveryZip;
+
+    @CreditCardNumber(message="Not a valid credit card number - correct credit card number required")
     private String ccNumber;
+
+    @Pattern(regexp="^(0$")
     private String ccExpiration;
+
+    @NotBlank(message="is required")
     private String ccCVV;
+
 
     private List<Taco> tacos = new ArrayList<>();
 
