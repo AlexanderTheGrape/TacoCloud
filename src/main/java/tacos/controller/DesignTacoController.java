dff530c30ccd501650f4b9853714338738314c65
Alex Monk <amonk446@gmail.com>
2023-09-11 21:47:03 +1000

    Added full repo class, refactored corresponding controller to use repo

--- src/main/java/tacos/controller/DesignTacoController.java
+++ src/main/java/tacos/controller/DesignTacoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+import tacos.data.IngredientRepository;
 import tacos.model.Ingredient;
 import tacos.model.Ingredient.Type;
 import tacos.model.Taco;
@@ -22,26 +23,22 @@ import java.util.stream.Collectors;
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
 
-    @ModelAttribute
-    public void addIngredientsToModel(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
+    private final IngredientRepository ingredientRepo;
 
+    public DesignTacoController(IngredientRepository ingredientRepo) {
+        this.ingredientRepo = ingredientRepo;
+    }
+
+    @ModelAttribute
+    public void addIngredientsToModel(Model model) { // TODO why are we using an empty spring model and adding ModelAttributes to it
+
+        Iterable<Ingredient> ingredients = ingredientRepo.findAll();
         Type[] types = Ingredient.Type.values();
         for (Type type : types) {
             model.addAttribute(
                     type.toString().toLowerCase(),
-                    filterByType(ingredients, type));
+                    filterByType((List<Ingredient>) ingredients, type)); // TODO apparently I didn't need to cast to list. How not to?
+
         }
     }
 
@@ -76,6 +73,9 @@ public class DesignTacoController {
         return "redirect:/orders/current";
     }
 
+    // TODO why are we not iterating through the ITERABLE ingredients iterable from above? why do we need to cast
+    // TODO ..the iterable to a list, then collect it back as an iterable?
+    // TODO if Interable can't .stream(), whats the point of using it at all? Why not List()?
     private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
         return ingredients
                 .stream()
--- src/main/java/tacos/data/JdbcIngredientRepository.java
+++ src/main/java/tacos/data/JdbcIngredientRepository.java
@@ -3,6 +3,12 @@ package tacos.data;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.jdbc.core.JdbcTemplate;
+import tacos.model.Ingredient;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public class JdbcIngredientRepository implements IngredientRepository {
@@ -11,5 +17,38 @@ public class JdbcIngredientRepository implements IngredientRepository {
 
     public JdbcIngredientRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    // Override not used here. TODO see if this causes any issues, when/when not to use override
+    public Iterable<Ingredient> findAll() {
+        return jdbcTemplate.query(
+                "select id, name, type from Ingredient",
+                this::mapRowToIngredient);
+    }
+
+    public Optional<Ingredient> findById(String id) {
+        List<Ingredient> results = jdbcTemplate.query(
+                "select id, name, type from Ingredient where id=?",
+                this::mapRowToIngredient, // TODO when is it better to use method reference operators (::)?
+                                            // TODO I thought lambda is better for readability
+                id); // TODO see how we do db reads in macquarie projects
+        return results.size() == 0 ? Optional.empty() : Optional.of(results.get(0));
+    }
+
+    public Ingredient save(Ingredient ingredient) {
+        jdbcTemplate.update(
+                "insert into Ingredient (id, name, type) values (?, ?, ?)",
+                ingredient.getId(),
+                ingredient.getName(),
+                ingredient.getType().toString());
+        return ingredient; // TODO why would we bother returning anything? Why not have a void method and possible error?
+    }
+
+    private Ingredient mapRowToIngredient(ResultSet row, int rowNum) throws SQLException {
+        return new Ingredient(
+                row.getString("id"),
+                row.getString("name"),
+                Ingredient.Type.valueOf(row.getString("type"))
+        );
     }
 }
